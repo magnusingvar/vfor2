6cc5284b6a59c19ecaa90cf90e3dc010ffd06b73
Magnus <magnusingvar@gmail.com>
2021-04-06 14:54:21 +0000

    lokaverkefni 5. og 6. april - lagaði login siðuna meira og nuna virkar hun a flest öllum skjastærðum fra öllu þvi sem eg hef profað

--- lokaverkefni/js/login.js
+++ lokaverkefni/js/login.js
@@ -1,38 +1,29 @@
 import usersJson from './users.js';
 
 const usersObject = JSON.parse(usersJson);
-
 const submitBtn = document.getElementById('login-btn');
 const pswrdChck = document.getElementById('pswrd-chck-box');
-
 const message = document.getElementById('message');
 
-submitBtn.addEventListener('click', ()=> {
+submitBtn.addEventListener('click', () => {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    for (let i = 0; i < usersObject.users.length; i += 1){
-        if(username == usersObject.users[i].name){
-            if(password == usersObject.users[i].password){
+    for (let i = 0; i < usersObject.users.length; i += 1) {
+        if (username == usersObject.users[i].name) {
+            if (password == usersObject.users[i].password) {
                 loginSuccessful('Login successful!');
                 return false;
-            } 
+            }
         } else {
             wrongLogin('Username or password is incorrect');
         }
-        
-        // Athugar hvort að username og password sé tómt 
-        if (username == ''){
+
+        // Athugar hvort að username og password sé tómt
+        if (username == '') {
             wrongLogin('Username cannot be empty');
-        } else if (password == ''){
+        } else if (password == '') {
             wrongLogin('Password cannot be empty');
         }
-
-        //if (username === usersObject.users[i].name){
-        //    message.innerHTML = 'This user exist';
-        //} else {
-        //    message.innerHTML = 'This user does not exist!';
-        //    console.log('user does not exist');
-        //}
     }
 });
 
@@ -47,11 +38,10 @@ pswrdChck.addEventListener('click', () => {
 
 // Falll sem birtir texta í errorMsg og eftir 4 sekúndur hreinsar hann textan út
 // og birtir '' sem merkir að það sé tómt, tekur einnig 1 af tries
-function wrongLogin(txt){
+function wrongLogin(txt) {
     message.innerHTML = txt;
     message.className = 'error';
-
-    setTimeout(function(){
+    setTimeout(function () {
         message.innerHTML = '';
         message.className = '';
     }, 6000);
@@ -59,10 +49,10 @@ function wrongLogin(txt){
 }
 
 // Fall sem segir notanda að innskráning hafi farið í gegn og breytir um vefslóð
-function loginSuccessful(txt){
+function loginSuccessful(txt) {
     message.innerHTML = txt;
     message.className = 'success';
-    setTimeout(function(){
+    setTimeout(function () {
         document.title = 'Game';
         message.innerHTML = '';
         message.className = '';
@@ -70,16 +60,14 @@ function loginSuccessful(txt){
 
     // Eftir að 1 sekúnda er liðin, þá keyrir það upp 'showScreen' sem birtir logged-in
     // div taggið.
-    setTimeout(function(){
+    setTimeout(function () {
         browser.hideScreens();
         browser.showScreen('startScreen');
-        //welcomeMsg.innerHTML = `Welcome back ${username.value}`;
-
     }, 1000);
-
     return false;
 }
 
-window.addEventListener('load', () => {
+window.addEventListener('load',() => {
+    document.cookie = 'SESSION=test';
     pswrdChck.checked = false;
 }, false);
